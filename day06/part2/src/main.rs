d448765a4f120e277c5585345fea372abf151fff
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-12-06 09:11:10 +0100

    No bigint library

--- day06/part2/src/main.rs
+++ day06/part2/src/main.rs
@@ -1,7 +1,5 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use num_bigint::{BigUint,ToBigUint};
-
 
 const DAY_COUNT : usize = 256;
 const MAX_FISH_COUNT : usize = 9;
@@ -10,9 +8,9 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let buf_reader = io::BufReader::new(file);
     let lines = buf_reader.lines();
-    let mut fishies : Vec<BigUint> = Vec::new();
+    let mut fishies : Vec<usize> = Vec::new();
     for _i in 0..MAX_FISH_COUNT {
-        fishies.push(0.to_biguint().unwrap());
+        fishies.push(0);
     }
 
     // parse
@@ -21,28 +19,27 @@ fn main() {
         let splits = unwrapped.split(",");
         for split in splits {
             let fish_count : usize = split.parse().unwrap();
-            fishies[fish_count] += 1.to_biguint().unwrap();
+            fishies[fish_count] += 1;
         }
     }
     for _day in 0..DAY_COUNT {
-        let mut new_fishies : Vec<BigUint> = Vec::new();
+        let mut new_fishies : Vec<usize> = Vec::new();
         for _i in 0..MAX_FISH_COUNT {
-            new_fishies.push(0.to_biguint().unwrap());
+            new_fishies.push(0);
         }
         for i in 0..MAX_FISH_COUNT {
             if i == 0 {
-                new_fishies[6] += &fishies[i];
-                new_fishies[8] += &fishies[i];
+                new_fishies[6] += fishies[i];
+                new_fishies[8] += fishies[i];
             } else {
-                new_fishies[i - 1] += &fishies[i];
+                new_fishies[i - 1] += fishies[i];
             }
         }
         fishies = new_fishies;
     }
-    let mut total_fish = 0.to_biguint().unwrap();
+    let mut total_fish = 0;
     for i in 0..MAX_FISH_COUNT {
-        total_fish += &fishies[i];
+        total_fish += fishies[i];
     }
     println!("{}", total_fish);
-
 }
