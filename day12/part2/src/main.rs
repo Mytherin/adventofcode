6a34905814070d39129d57c024f417d46db9561d
Mark Raasveldt <mark.raasveldt@gmail.com>
2021-12-13 07:51:55 +0100

    day12 and day13

---
+++ day12/part2/src/main.rs
@@ -0,0 +1,82 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+struct Node {
+    name : String,
+    neighbors : Vec<usize>,
+    is_small : bool,
+    is_start_or_end : bool,
+}
+
+fn find_node(name : &String, nodes : &Vec<Node>) -> usize {
+    for i in 0..nodes.len() {
+        if nodes[i].name == *name {
+            return i;
+        }
+    }
+    return nodes.len();
+}
+
+fn add_node(name : String, nodes : &mut Vec<Node>) -> usize {
+    let index = find_node(&name, nodes);
+    if index < nodes.len() {
+        return index;
+    }
+    let upper = name.to_ascii_uppercase();
+    let is_small = upper != name;
+    let is_start_or_end = name == "start" || name == "end";
+    nodes.push(Node { name : name, neighbors : Vec::new(), is_small : is_small, is_start_or_end : is_start_or_end});
+    return nodes.len() - 1;
+}
+
+fn traverse_cave(current_pos : usize, goal : usize, mut visited : Vec<bool>, nodes : &Vec<Node>, double_visited_small_cave : bool) -> usize {
+    if current_pos == goal {
+        return 1;
+    }
+    if nodes[current_pos].is_small {
+        visited[current_pos] = true;
+    }
+    let mut paths = 0;
+    for neighbor in &nodes[current_pos].neighbors {
+        let mut double_visit = double_visited_small_cave;
+        if visited[*neighbor] {
+            if !double_visited_small_cave && !nodes[*neighbor].is_start_or_end {
+                double_visit = true;
+            } else {
+                // already visited on this path
+                continue;
+            }
+        }
+        paths += traverse_cave(*neighbor, goal, visited.clone(), nodes, double_visit);
+    }
+    return paths;
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let buf_reader = io::BufReader::new(file);
+    let lines = buf_reader.lines();
+    let mut nodes : Vec<Node> = Vec::new();
+    // parse
+    for line in lines {
+        let unwrapped = line.unwrap();
+        let splits : Vec<&str> = unwrapped.split('-').collect();
+        let left_index = add_node(splits[0].to_string(), &mut nodes);
+        let right_index = add_node(splits[1].to_string(), &mut nodes);
+        nodes[left_index].neighbors.push(right_index);
+        nodes[right_index].neighbors.push(left_index);
+    }
+    // where is the start and where is the end
+    let start_name : String = "start".to_string();
+    let end_name : String = "end".to_string();
+    let start_index = find_node(&start_name, &nodes);
+    let end_index = find_node(&end_name, &nodes);
+
+    // perform the traversal
+    let mut visited : Vec<bool> = Vec::new();
+    for _n in 0..nodes.len() {
+        visited.push(false);
+    }
+    let path_count = traverse_cave(start_index, end_index, visited, &nodes, false);
+    println!("{}", path_count);
+}
---
+++ day13/part1/src/main.rs
@@ -0,0 +1,97 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+struct Point {
+    x : usize,
+    y : usize,
+}
+
+fn fold_map(fold : &Point, map : &mut Vec<Vec<bool>>, width : &mut usize, height : &mut usize) {
+    if fold.x > 0 {
+        // horizontal fold
+        for x in fold.x..*width + 1 {
+            for y in 0..*height+1 {
+                if map[x][y] {
+                    map[fold.x - (x - fold.x)][y] = true;
+                    map[x][y] = false;
+                }
+            }
+        }
+        *width = fold.x;
+    } else if fold.y > 0 {
+        // vertical fold
+        // horizontal fold
+        for x in 0..*width + 1 {
+            for y in fold.y..*height+1 {
+                if map[x][y] {
+                    map[x][fold.y - (y - fold.y)] = true;
+                    map[x][y] = false;
+                }
+            }
+        }
+        *height = fold.y;
+    } else {
+        panic!("empty fold!?");
+    }
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let buf_reader = io::BufReader::new(file);
+    let lines = buf_reader.lines();
+    let mut points : Vec<Point> = Vec::new();
+    let mut folds : Vec<Point> = Vec::new();
+    let mut width : usize = 0;
+    let mut height : usize = 0;
+    // parse
+    for line in lines {
+        let unwrapped = line.unwrap();
+        if unwrapped.len() == 0 {
+            continue;
+        }
+        if unwrapped.starts_with("fold along ") {
+            let splits : Vec<&str> = unwrapped.split('=').collect();
+            let location = splits[1].parse().unwrap();
+            if unwrapped.starts_with("fold along x") {
+                folds.push(Point { x: location, y : 0 });
+            } else {
+                folds.push(Point { x: 0, y : location });
+            }
+        } else {
+            let splits : Vec<&str> = unwrapped.split(',').collect();
+            let x = splits[0].parse().unwrap();
+            let y = splits[1].parse().unwrap();
+            if x > width {
+                width = x;
+            }
+            if y > height {
+                height = y;
+            }
+            points.push(Point { x : x, y : y });
+        }
+    }
+    // create the empty map
+    let mut map : Vec<Vec<bool>> = Vec::new();
+    for _x in 0..width+1 {
+        let mut vertical : Vec<bool> = Vec::new();
+        for _y in 0..height+1 {
+            vertical.push(false);
+        }
+        map.push(vertical);
+    }
+    for point in points {
+        map[point.x][point.y] = true;
+    }
+    // perform the fold
+    fold_map(&folds[0], &mut map, &mut width, &mut height);
+
+    let mut num_points = 0;
+    for x in 0..width+1 {
+        for y in 0..height+1 {
+            if map[x][y] {
+                num_points += 1;
+            }
+        }
+    }
+    println!("{}", num_points);
+}
